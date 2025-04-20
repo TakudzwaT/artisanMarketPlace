bf1a33e9c335c8292f14b4c8284299612d89c738
Takudzwa <takudzwamhizha132@gmail.com>
2025-04-20 22:47:59 +0200

    Seller order tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Card from './card';
 import Navigation from "./nav";
 import BuyerTrack from './Buyer';
+import SellerTrack from './sellerOrders';
 function App() {
 
   return (
     <>
-      <BuyerTrack/>
+    
       
 
+    <SellerTrack/>
     </>
   );
 }
