08da8ba69637e0730835314ace822c385e3941d2
Takudzwa <2742390@students.wits.ac.za>
2025-05-25 04:02:38 +0200

    tests

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true, // fallback to polling
-      interval: 1000, // check every second
-      ignored: [
-        
-      ]
-    }
-  }
+  
 })
