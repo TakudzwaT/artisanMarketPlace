0250baa2a9f85d36588049c8f35d329b92f2fb9b
Takudzwa <2742390@students.wits.ac.za>
2025-05-25 04:18:17 +0200

    all tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,5 +7,22 @@ module.exports = {
   },
   transform: {
     "^.+\\.[jt]sx?$": "babel-jest"
-  }
+  },
+
+  // Add coverage config
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.test.{js,jsx,ts,tsx}",
+    "!src/**/index.{js,jsx,ts,tsx}"
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text"],
+
+  // Optional: if you want to ignore test files from coverage, else you can remove this
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/",
+    "/coverage/"
+  ]
 };
